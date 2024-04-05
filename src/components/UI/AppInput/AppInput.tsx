f74207428a5a0fc2cda6fe02f87f25c03cdd4e52
STASKE <STASKE4IT@gmail.com>
2024-04-05 21:23:17 +0500

    text

---
+++ src/components/UI/AppInput/AppInput.tsx
@@ -0,0 +1,8 @@
+type AppInputProps = {
+  inputType: string;
+  inputPlaceholder: string;
+};
+
+export const AppInput = ({ inputPlaceholder, inputType }: AppInputProps) => {
+  return <input type={inputType} placeholder={inputPlaceholder} />;
+};
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,32 @@
+import { Navbar } from "../../components/NavBar/Navbar";
+import { FollowersList } from "../../components/FollowersList/FollowersList";
+import { WhatsNew } from "../../components/WhatsNew/Whatsnew";
+import { History } from "../../components/History/History";
+import { AppPost } from "../../components/AppPost/AppPost";
+import { AppRepost } from "../../components/AppRepost/AppRepost";
+import { Friendslist } from "../../components/FriendsList/Friendslist";
+import { MusicBlock } from "../../components/MusicBlock/Musicblock";
+import { Header } from "../../components/UI/Header/Header";
+import "./MainPage.scss";
+
+export const MainPage = () => {
+  return (
+    <div className="MainPage">
+      <Header/>
+      <aside className="LeftSide">
+        <Navbar/>
+        <FollowersList/>
+      </aside>
+      <main className="Main">
+        <WhatsNew/>
+        <History/>
+        <AppPost/>
+        <AppRepost/>
+      </main>
+      <aside className="RightSide">
+        <Friendslist/>
+        <MusicBlock/>    
+      </aside>
+    </div>
+  );
+};
