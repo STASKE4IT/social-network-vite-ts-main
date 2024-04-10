ff99e5e4c9e25325888df72709f5078be02751ca
STASKE <STASKE4IT@gmail.com>
2024-04-10 12:10:56 +0500

    text

--- src/components/FriendBlock/FriendBlock.tsx
+++ src/components/FriendBlock/FriendBlock.tsx
@@ -1,48 +1,23 @@
+import { AppHeading } from "../Typography/AppHeading/AppHeading";
+import { FriendElem } from "./FriendElem";
+import { data } from "./data";
+
 export const FriendBlock = () => {
-    return(
-        <div className="FriendsBlock">
-          <div className="Friends__title">
-            <h2>Друзья</h2>
-            <span className="count">130</span>
+  return (
+    <div className="FriendsBlock">
+      <div className="Friends__title">
+      <AppHeading headingText="Друзья" headingType="h2" />
+            <span className="count">{data.length}</span>
           </div>
           <div className="Friends__wrapper">
-            <div className="friend">
-              <img src="./img/users/aleksandr-maykov.jpeg" alt="Friend" />
-              <span className="friend__name">Александр</span>
-            </div>
-            <div className="friend">
-              <img src="./img/users/sofia-kodra.jpeg" alt="Friend" />
-              <span className="friend__name">Софья</span>
-            </div>
-            <div className="friend">
-              <img src="./img/users/andrey-kashirskiy.jpeg" alt="Friend" />
-              <span className="friend__name">Андрей</span>
-            </div>
-            <div className="friend">
-              <img src="./img/users/darya-gertner.jpeg" alt="Friend" />
-              <span className="friend__name">Дарья</span>
-            </div>
-            <div className="friend">
-              <img src="./img/users/denis-frolov.jpeg" alt="Friend" />
-              <span className="friend__name">Денис</span>
-            </div>
-            <div className="friend">
-              <img src="./img/users/mark-krahmalev.jpeg" alt="Friend" />
-              <span className="friend__name">Марк</span>
-            </div>
-            <div className="friend">
-              <img src="./img/users/katarina-fisher.jpeg" alt="Friend" />
-              <span className="friend__name">Катарина</span>
-            </div>
-            <div className="friend">
-              <img src="./img/users/viktoria.jpeg" alt="Friend" />
-              <span className="friend__name">Виктория</span>
-            </div>
-            <div className="friend">
-              <img src="./img/users/gleb.jpeg" alt="Friend" />
-              <span className="friend__name">Глеб</span>
-            </div>
-          </div>
-        </div>
-    )
-}
+      {data &&
+        data.map((elem) => (
+          <FriendElem 
+          imgLink={elem.imgLink} 
+          mainText={elem.mainText} 
+          />
+        ))} 
+      </div>
+    </div>
+  );
+};
--- src/components/FriendsList/Friendslist.tsx
+++ src/components/FriendsList/Friendslist.tsx
@@ -1,34 +1,23 @@
+import { AppHeading } from "../Typography/AppHeading/AppHeading";
+import { FriendslistElem } from "./FriendslistElem";
+import { data } from "./data";
+
 export const Friendslist = () => {
-    return(
-        <div className="List">
-          <div className="List__title">
-            <h2>Близкие друзья</h2>
-            <span className="count">123</span>
-          </div>
-          <div className="UserElem">
-            <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
-            <div className="user__description">
-              <p className="main__text">Александр Майков</p>
-              <p className="secondary__text _online">Онлайн</p>
-            </div>
-            <span className="Badge">3</span>
-          </div>
-          <div className="UserElem">
-            <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
-            <div className="user__description">
-              <p className="main__text">Александр Майков</p>
-              <p className="secondary__text _online">Онлайн</p>
-            </div>
-            <span className="Badge">3</span>
-          </div>
-          <div className="UserElem">
-            <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
-            <div className="user__description">
-              <p className="main__text">Александр Майков</p>
-              <p className="secondary__text _online">Онлайн</p>
-            </div>
-            <span className="Badge">3</span>
-          </div>
-        </div>
-    )
-}
+  return (
+    <div className="List">
+      <div className="List__title">
+        <AppHeading headingText="Близкие друзья" headingType="h2" />
+        <span className="count">{data.length}</span>
+      </div>
+      {data &&
+        data.map((elem) => (
+          <FriendslistElem
+            imgLink={elem.imgLink}
+            mainText={elem.mainText}
+            secondText={elem.secondText}
+            badge={elem.badge}
+          />
+        ))}
+    </div>
+  );
+};
